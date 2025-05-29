85a8a7f77d272109c660fc170a4d56e384f8d32b
hskhsmm <hskhsmm@naver.com>
2025-05-30 02:11:00 +0900

    feat: repository 테스트, 로그 확인

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -13,8 +13,12 @@ public class Member {
 
     @Id @GeneratedValue
     private Long id;
-
     private String username;
 
+    protected Member() {
+    } //기본 생성자를 private으로 막으면 객체를 못만들어냄.
 
+    public Member(String username) {
+        this.username = username;
+    }
 }
