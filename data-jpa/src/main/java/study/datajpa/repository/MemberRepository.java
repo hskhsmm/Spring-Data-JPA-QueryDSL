31a18e57cc4d0b95b3c08be2b6cf83cabc35877c
hskhsmm <hskhsmm@naver.com>
2025-06-02 23:52:16 +0900

    feat: 파라미터 바인딩

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -27,4 +27,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     //리스트에서 DTO 조회
     @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
     List<MemberDto> findMemberDto();
+
+    //파라미터 바인딩 해보기
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
 }
--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -11,6 +11,7 @@ import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,6 +25,8 @@ class MemberRepositoryTest {
     MemberRepository memberRepository;
     @Autowired
     TeamRepository teamRepository;
+    @Autowired
+    private MemberJpaRepository memberJpaRepository;
 
     @Test
     public void testMember() {
@@ -133,6 +136,19 @@ class MemberRepositoryTest {
             System.out.println("dto = " + dto);
         }
 
+    }
+
+    @Test
+    public void findByNames() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB")); // ✅
+        for (Member member : result) {
+            System.out.println("member: " + member);
+        }
     }
 
 }
