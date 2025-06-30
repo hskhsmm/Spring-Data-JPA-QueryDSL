1562f657442dbb6b5c2f1a27076988f3170fe51d
hskhsmm <hskhsmm@naver.com>
2025-06-30 23:04:19 +0900

    feat: join 연습

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -225,6 +225,31 @@ public class QuerydslBasicTest {
 
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35); //(30 + 40) / 2
+
+    }
+
+    @Test
+    public void join() {
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result).extracting("username").containsExactly("member1", "member2");
+    }
+
+    @Test
+    public void theta_join() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        queryFactory
+                .select(member)
+                .from(member,team)
+                .where(member.username.eq(team.name))
+                .fetch();
     }
 
 
